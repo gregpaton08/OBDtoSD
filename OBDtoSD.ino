21d8a570f98e895228182fc204083a5044cfdbbc
gregpaton08 <gregpaton08@gmail.com>
2013-04-03 10:54:17 -0400

    [add] Added overloaded logPID() function that accepts MODE and PID as hex values.

--- OBDtoSD.ino
+++ OBDtoSD.ino
@@ -28,7 +28,9 @@ void setup() {
   OBDLog.println("");
   OBDLog.println("");
    
+  // wait a second
   delay(1000);
+  // okay
   
   startTime = millis();
 }
@@ -36,23 +38,34 @@ void setup() {
 
 void loop() {
   // RPM
-  logPID("01 0C");
+  //logPID("01 0C");
+  logPID(0x01, 0x0C);
   // Speed
-  logPID("01 0D");
+  //logPID("01 0D");
+  logPID(0x01, 0x0D);
   // MAF
-  logPID("01 10");
+  //logPID("01 10");
+  logPID(0x01, 0x10);
   // Throttle Position
-  logPID("01 11");
+  //logPID("01 11");
+  logPID(0x01, 0x11);
+}
+
+
+void logPID(char *PID) {
+  Serial.write(PID);
+  Serial.write(" 1\r\n");  
   
-  /*while (true) {
+  while (true) {
     // wait until serial data is available
     while(Serial.available() == 0); 
     // seek to beginning of returned data
     while(false == Serial.find("4"));
-    //delay(200);
+    // print time and '4' that was skipped by Serial.find()
     OBDLog.print(millis() - startTime);
     OBDLog.print(", ");
     OBDLog.write('4');
+    // loop until new line character found
     while (true) {
       unsigned char c = Serial.read();
       if (c == (unsigned char)-1) continue;
@@ -60,15 +73,42 @@ void loop() {
       OBDLog.write(c);
     }
     OBDLog.println("");
+    // flush data to SD card
     OBDLog.flush();
     break;
-  }*/
+  }
 }
 
 
-void logPID(char *PID) {
-  Serial.write(PID);
-  Serial.write(" 1\r\n");  
+void logPID(byte mode, byte pid) {
+  // Query PID
+  switch (mode) {
+    case 0x01:
+      switch (pid) {
+        // RPM
+        case 0x0C:
+          Serial.write("01 0C 1\r\n"); 
+          break;
+        // Speed
+        case 0x0D:
+          Serial.write("01 0D 1\r\n"); 
+          break;
+        // MAF
+        case 0x10:
+          Serial.write("01 10 1\r\n"); 
+          break;
+        // Throttle Position
+        case 0x11:
+          Serial.write("01 11 1\r\n"); 
+          break;
+        default:
+          return;
+      }
+      break;
+    default:
+      return;
+  }
+  
   
   while (true) {
     // wait until serial data is available
