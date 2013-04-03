907d40aee246b87b343c09de5aaf3a08eb1ea3eb
gregpaton08 <gregpaton08@gmail.com>
2013-04-03 10:42:52 -0400

    [mod] Now logs RPM, Speed, MAF, and Throttle Position.

--- OBDtoSD.ino
+++ OBDtoSD.ino
@@ -4,6 +4,7 @@
 File OBDLog;
 unsigned long startTime;
 
+
 void setup() {
   Serial.begin(38400);
   
@@ -34,11 +35,16 @@ void setup() {
 
 
 void loop() {
-  Serial.write("01 0C 1\r\n");
+  // RPM
+  logPID("01 0C");
+  // Speed
+  logPID("01 0D");
+  // MAF
+  logPID("01 10");
+  // Throttle Position
+  logPID("01 11");
   
-  while (true) {
-    //OBDLog.println("---LOOP BEGIN---");
-    
+  /*while (true) {
     // wait until serial data is available
     while(Serial.available() == 0); 
     // seek to beginning of returned data
@@ -49,12 +55,39 @@ void loop() {
     OBDLog.write('4');
     while (true) {
       unsigned char c = Serial.read();
-      if (c == -1) continue;
+      if (c == (unsigned char)-1) continue;
       if (c == '\n' || c == '\r') break;
       OBDLog.write(c);
     }
     OBDLog.println("");
-    //OBDLog.println("---LOOP END---");
+    OBDLog.flush();
+    break;
+  }*/
+}
+
+
+void logPID(char *PID) {
+  Serial.write(PID);
+  Serial.write(" 1\r\n");  
+  
+  while (true) {
+    // wait until serial data is available
+    while(Serial.available() == 0); 
+    // seek to beginning of returned data
+    while(false == Serial.find("4"));
+    // print time and '4' that was skipped by Serial.find()
+    OBDLog.print(millis() - startTime);
+    OBDLog.print(", ");
+    OBDLog.write('4');
+    // loop until new line character found
+    while (true) {
+      unsigned char c = Serial.read();
+      if (c == (unsigned char)-1) continue;
+      if (c == '\n' || c == '\r') break;
+      OBDLog.write(c);
+    }
+    OBDLog.println("");
+    // flush data to SD card
     OBDLog.flush();
     break;
   }
